c223227729d25ad989ea5129f02ada982ca51baa
Soha Rahman <studentinsha12@gmail.com>
2017-11-13 05:59:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber===undefined)
-  if(i===cart.length)
    console.log("Sorry, we don't have a credit card on file for you.");
   else {
 
