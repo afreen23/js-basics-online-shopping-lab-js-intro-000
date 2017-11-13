2dbbbc141410d650973a90bf3e95f84eff474827
Soha Rahman <studentinsha12@gmail.com>
2017-11-13 03:32:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemPrice=Math.floor(Math.random()*100);
+  var newobj= Object.assign({},{[item]: itemPrice});
+  cart.push(newobj);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
