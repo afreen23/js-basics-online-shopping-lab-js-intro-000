14292a4089255665edbb39e460043fa19e877b50
Soha Rahman <studentinsha12@gmail.com>
2017-11-13 04:06:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
   else {
     var a=Object.keys(cart);
     for(i=0;i<l;i++) {
-      console.log(`In you cart, you have ${a[i]} at ${cart[a[i]]}${l==1||l==?'.':(i==l-1?'.':',')}`);
+      console.log(`In you cart, you have ${a[i]} at ${cart[a[i]]}${l==1||i==l-1?'.':(i==l-2?' and ':',')}`);
     }
   }
 }
